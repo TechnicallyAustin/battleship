995617fdd0df4ae92c39fa991ef0cdbbeff6c864
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-06-05 15:31:11 -0700

    working on tests

--- tests/game.test.js
+++ tests/game.test.js
@@ -1,11 +1,54 @@
-// Gameboard is defined as a function
-    // calls ship factory function to place ships
-    // places the ships via coordinates as arguments
-    
-    // recieve attack function
-        // takes a pair of coordinates
-        // determines if the attack hits or misses
-        // sends hit() to the correct ship || // records a missed shot
+const gameboard = require("../src/game/gameboard")
 
-    // gameboard can keep track of missed attacks and display them
-    // gameboard can report if all ships have been sunk or not.
+    describe("Gameboard", () => {
+        test("Gameboard() is defined", () => {
+            expect().toBe();
+        });
+
+        test("Gameboard calls the ship factory function", () => {
+            expect().toBe();
+        });
+
+        test("Gameboard has two arguments. ship and coordinates", () => {
+            expect().toBe();
+        });
+
+        test("Gameboard places thes ships given the coordinaates", () => {
+            expect().toBe();
+        });
+
+        test("Gameboard keeps a record of missed attacks",() =>{
+            expect().toBe();
+        })
+        
+        test("Gameboard displays missed attacks", () => {
+            expect().toBe();
+        });
+
+        test("Gameboard displays landed attacks", () => {
+          expect().toBe();
+        });
+
+        test("Gameboard reports when all ships are sunk", () => {
+          expect().toBe();
+        });
+
+        describe("Recieve Attack", () => {
+            test("recieveAttack() is defined", () => {
+                expect().toBe();
+            });
+            test("recieveAttack has two arguments. coordinate 1 and coordinate 2", () => {
+                expect().toBe();
+            });
+
+            test("recieveAttack returns a boolean for hit or miss",() =>{
+                expect().toBe();
+            });
+            test("recieveAttack adds a hit to the correct ship or records a missed shot",() =>{
+                expect().toBe();
+            });
+            test("recieveAttack displays landed attacks", () => {
+                expect().toBe();
+            });
+        })
+    })
--- tests/player.test.js
+++ tests/player.test.js
@@ -1,5 +1,14 @@
 // player exists
 
+
 // player can make random moves
     // avoids repeating moves
     // knows game rules
+
+const player = require("../src/player/player")
+
+describe("Player", () => {
+    test("test case", ()=>{
+
+    })
+})
